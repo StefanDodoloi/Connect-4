40ab537335991f8aa52e63f2b9476a95b08a45f8
StefanDodoloi <153545504+StefanDodoloi@users.noreply.github.com>
2024-02-01 10:54:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,55 +1,97 @@
-const cells = document.querySelectorAll('.cell');
-const gameOver = document.querySelector('#gameOver');
-const winConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-let options;
-let currentPlayer = 'X';
+let currentPlayer = 'Red';
+let gameOver = false;
+let board = [];
+let currentColumnsBottomRow = [5, 5, 5, 5, 5, 5, 5];
+const rows = 6, columns = 7;
 
-newGame();
-
-function newGame() {
-    cells.forEach(cell => cell.addEventListener('click', cellClicked));
-    currentPlayer = 'X';
-    options = ['', '', '', '', '', '', '', '', ''];
-    cells.forEach(cell => cell.textContent = '');
-    gameOver.textContent = '';
+for (let r = 0; r < rows; r++) {
+    let row = [];
+    for (let c = 0; c < columns; c++) {
+        row.push('');
+        let cell = document.createElement('div');
+        cell.id = r.toString() + '-' + c.toString();
+        cell.classList.add('cell');
+        cell.addEventListener('click', setPiece);
+        document.getElementById('board').append(cell);
+    }
+    board.push(row);
 }
 
-function cellClicked() {
-    const cellIndex = this.getAttribute('cellIndex'); 
-    if (options[cellIndex] !== '' || gameOver.textContent !== '') {
+function setPiece() {
+    if (gameOver) {
         return;
     }
-    options[cellIndex] = currentPlayer;
-    this.textContent = currentPlayer;
+    let coords = this.id.split('-');
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+    r = currentColumnsBottomRow[c]; 
+    board[r][c] = currentPlayer;
+    let cell = document.getElementById(r.toString() + "-" + c.toString());
+    if (currentPlayer === 'Red') {
+        cell.classList.add('red-piece');
+        currentPlayer = 'Yellow';
+    }
+    else {
+        cell.classList.add('yellow-piece');
+        currentPlayer = 'Red';
+    }
+    currentColumnsBottomRow[c] -= 1;
     checkWinner();
 }
 
 function checkWinner() {
-    let gameEnd = false;
-    for (let i = 0; i < winConditions.length; i++) {
-        const condition = winConditions[i];
-        const cellA = options[condition[0]];
-        const cellB = options[condition[1]];
-        const cellC = options[condition[2]];
-        if (cellA == cellB && cellB == cellC && cellA !== '') {
-            gameEnd = true;
-            break;
+    for (let r = 0; r < rows; r++) {
+         for (let c = 0; c < columns - 3; c++){
+            if (board[r][c] != '' && board[r][c] === board[r][c+1] && board[r][c+1] === board[r][c+2] && board[r][c+2] === board[r][c+3]) {
+                setWinner();
+                return;
+            }
         }
     }
-    if (gameEnd) {
-        gameOver.textContent = `${currentPlayer} wins!`;
-    } else if (!options.includes('')) {
-        gameOver.textContent = `Draw!`;
-    } else {
-        currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows - 3; r++) {
+            if (board[r][c] != '' && board[r][c] === board[r+1][c] && board[r+1][c] === board[r+2][c] && board[r+2][c] === board[r+3][c]) {
+                setWinner();
+                return;
+            } 
+        }
     }
+    for (let r = 0; r < rows - 3; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != '' && board[r][c] === board[r+1][c+1] && board[r+1][c+1] === board[r+2][c+2] && board[r+2][c+2] === board[r+3][c+3]) {
+                setWinner();
+                return;
+            }
+        }
+    }
+    for (let r = 0; r < rows - 3; r++) {
+        for (let c = 3; c <= columns; c++) {
+            if (board[r][c] != '' && board[r][c] === board[r+1][c-1] && board[r+1][c-1] === board[r+2][c-2] && board[r+2][c-2] === board[r+3][c-3]) {
+                setWinner();
+                return;
+            }
+        }
+    }
+    let whiteCells = 0;
+    for (let r = 0; r < rows; ++r) {
+        for (let c = 0; c < columns; ++c) {
+            if (board[r][c] === '') {
+                ++whiteCells;
+            }
+        }
+    }
+    if (whiteCells === 0) {
+        winner.innerText = 'It\'s a draw!';
+        gameOver = true;
+    }
+}
+
+function setWinner() {
+    let winner = document.getElementById('winner');
+    if (currentPlayer !== 'Red') {
+        winner.innerText = 'Red Wins!';             
+    } else {
+        winner.innerText = 'Yellow Wins!';
+    }
+    gameOver = true;
 }
